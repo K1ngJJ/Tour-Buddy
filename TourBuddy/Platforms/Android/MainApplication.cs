3de54feee89bd15492abeca4ee469fbfebf7c6b7
ZRNJJ <pacheco.kingjayjay@minsu.edu.ph>
2025-04-08 13:48:10 +0800

    Add project files.

---
+++ TourBuddy/Platforms/Android/MainApplication.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Runtime;
+using System.Threading.Tasks;
+
+namespace TourBuddy
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp()
+        {
+            // Run the asynchronous method and wait for the result
+            return Task.Run(() => MauiProgram.CreateMauiApp()).Result;
+        }
+    }
+}
+
+
+
+//using Android.App;
+//using Android.Runtime;
+
+//namespace TourBuddy
+//{
+//    [Application]
+//    public class MainApplication : MauiApplication
+//    {
+//        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+//            : base(handle, ownership)
+//        {
+//        }
+
+//        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+//    }
+//}
